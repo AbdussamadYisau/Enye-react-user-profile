95cd98c8c8eda23d6310eb85b428258b805becc9
AbdussamadYisau <sammieyisau@gmail.com>
2021-01-14 14:09:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, {useEffect, useState } from 'react'
+import {  } from "module";
+import axios from 'axios'
+import Records from './Components/Records';
+import Pagination from './Components/Pagination';
+import { Container} from 'react-bootstrap';
+
+
+function App() {
+  const [records, setRecords] = useState([]);
+  const [keysArr, setKeysArr] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(20);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://api.enye.tech/v1/challenge/records');
+      setRecords(res.data.records.profiles);
+      setKeysArr(Object.keys(res.data.records.profiles[0]));
+      setLoading(false);
+      console.log(res.data.records.profiles);
+    }
+
+    fetchPosts();
+  }, []); 
+  
+  //  Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = records.slice(indexOfFirstPost, indexOfLastPost);
+
+  //  Change Page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  
+  return (
+      <>
+        <div>
+          <Container>
+            <Records records ={currentPosts} keysArr = {keysArr} loading = {loading} />
+            <Pagination postsPerPage={postsPerPage} totalPosts={records.length} paginate={paginate} />
+          </Container>
+        </div>
+      </>
+  );
+
+}
+
+export default App;
+
---
+++ src/Components/Records.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Table} from 'react-bootstrap';
+
+const Records = ({records, keysArr, loading}) => {
+    if(loading) {
+        return <h2>Loading...</h2>;
+    }
+
+    return (
+        <Table striped bordered hover responsive>
+        <thead>
+            <tr>
+            <th>#</th>
+            {keysArr.map((keyName, index) => (
+                <th key={index}>{keyName}</th>
+            ))}
+            </tr>
+        </thead>
+        <tbody>
+            {records.map((each, index) => (
+            <tr key={index}>
+                <td>{index + 1}</td>
+                {Object.entries(each).map(([keyName, val], index) => ( 
+                <td key={index}>{val}</td>
+                ))}
+            </tr>
+            ))}
+        </tbody>
+        </Table>
+    );
+};
+
+export default Records; 
